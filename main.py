2f4d78d3ce35d33f0ab85e8cbec8f4d79c44df4b
Aviral Garg <12901566+gaviral@users.noreply.github.com>
2021-02-01 23:09:39 -0800

    minor refactor

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import speech_recognition as sr
 
 
 def say(text):
-    print(f"USER: {transcript}")
+    print(text)
     subprocess.call(['say', text])
 
 
-if __name__ == '__main__':
-    # initialize
+def listen():
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
 
-    # stt
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
-        transcript = recognizer.recognize_google(audio)
+        return recognizer.recognize_google(audio)
 
-    # log & speak
+
+if __name__ == '__main__':
+    transcript = listen()
     say(f"You said: {transcript}")
